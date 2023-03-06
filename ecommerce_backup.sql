54c2b82fce6536035e816862eead436659865c9a
gustavoloppes <124110090+gustavoloppes@users.noreply.github.com>
2023-03-06 16:01:08 -0400

    Add files via upload

---
+++ ecommerce_backup.sql
@@ -0,0 +1,314 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecommerce
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `Fname` varchar(10) DEFAULT NULL,
+  `Minit` char(3) DEFAULT NULL,
+  `Lname` varchar(20) DEFAULT NULL,
+  `CPF` char(11) NOT NULL,
+  `Address` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`),
+  UNIQUE KEY `unique_cpf_cliente` (`CPF`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'maria','m','silva','123456789','rua silva de prata 29, Garangola -Cidade das flores'),(2,'matheus','o','pimentel','123456798','rua costa de ouro 29, centro -Cidade das flores'),(3,'ricardo','f','silva','234567891','rua dai luiz 56, centro -Cidade das flores'),(4,'julia','s','franca','345678912','rua yana lopes 17, Garangola -Cidade das flores'),(5,'roberta','g','assis','456789123','rua daniele santos 15, centro -Cidade das flores'),(6,'isabela','m','cruz','567891234','rua joao de assis 89, Garangola -Cidade das flores');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `idOrder` int NOT NULL AUTO_INCREMENT,
+  `idOrderCliente` int DEFAULT NULL,
+  `oderStatus` enum('cancelado','confirmado','em processamento') DEFAULT 'em processamento',
+  `orderDescription` varchar(255) DEFAULT NULL,
+  `sendValue` float DEFAULT '10',
+  `paymentCash` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`idOrder`),
+  KEY `fk_orders_cliente` (`idOrderCliente`),
+  CONSTRAINT `fk_orders_cliente` FOREIGN KEY (`idOrderCliente`) REFERENCES `cliente` (`idCliente`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (9,1,'em processamento','compra via aplicativo',NULL,1),(10,2,'em processamento','compra via aplicativo',NULL,50),(11,3,'confirmado',NULL,NULL,1),(12,4,'em processamento','compra via web site',NULL,15),(13,2,'em processamento','compra via aplicativo',NULL,1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `idProduct` int NOT NULL AUTO_INCREMENT,
+  `Pname` varchar(10) NOT NULL,
+  `Classification_kids` tinyint(1) DEFAULT '0',
+  `category` enum('eletronico','vstuario','brinquedos','alimento','moveis') NOT NULL,
+  `avaliacao` float DEFAULT '0',
+  `size` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`idProduct`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (7,'fone',0,'eletronico',4,NULL),(8,'barbie',1,'brinquedos',3,NULL),(9,'body c.',1,'vstuario',5,NULL),(10,'microfone',0,'eletronico',4,NULL),(11,'sofa',0,'moveis',3,'3x57x80'),(12,'fire stick',0,'eletronico',3,NULL);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productorder`
+--
+
+DROP TABLE IF EXISTS `productorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productorder` (
+  `idPOproduct` int NOT NULL,
+  `idPOorder` int NOT NULL,
+  `poQuantity` int DEFAULT '1',
+  `poStatus` enum('disponivel','sem estoque') DEFAULT 'disponivel',
+  PRIMARY KEY (`idPOproduct`,`idPOorder`),
+  KEY `fk_productorder_product` (`idPOorder`),
+  CONSTRAINT `fk_productorder_product` FOREIGN KEY (`idPOorder`) REFERENCES `orders` (`idOrder`),
+  CONSTRAINT `fk_productorder_seller` FOREIGN KEY (`idPOproduct`) REFERENCES `product` (`idProduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productorder`
+--
+
+LOCK TABLES `productorder` WRITE;
+/*!40000 ALTER TABLE `productorder` DISABLE KEYS */;
+INSERT INTO `productorder` VALUES (7,9,2,NULL),(8,10,1,NULL),(9,11,1,NULL);
+/*!40000 ALTER TABLE `productorder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productseller`
+--
+
+DROP TABLE IF EXISTS `productseller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productseller` (
+  `idPseller` int NOT NULL,
+  `idPproduct` int NOT NULL,
+  `prodQuantity` int DEFAULT '1',
+  PRIMARY KEY (`idPseller`,`idPproduct`),
+  KEY `fk_product_product` (`idPproduct`),
+  CONSTRAINT `fk_product_product` FOREIGN KEY (`idPproduct`) REFERENCES `product` (`idProduct`),
+  CONSTRAINT `fk_product_seller` FOREIGN KEY (`idPseller`) REFERENCES `seller` (`idSeller`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productseller`
+--
+
+LOCK TABLES `productseller` WRITE;
+/*!40000 ALTER TABLE `productseller` DISABLE KEYS */;
+INSERT INTO `productseller` VALUES (1,7,80),(2,8,10);
+/*!40000 ALTER TABLE `productseller` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productstorage`
+--
+
+DROP TABLE IF EXISTS `productstorage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productstorage` (
+  `idProdStorage` int NOT NULL AUTO_INCREMENT,
+  `storageLocation` varchar(255) DEFAULT NULL,
+  `quantity` int DEFAULT '0',
+  PRIMARY KEY (`idProdStorage`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productstorage`
+--
+
+LOCK TABLES `productstorage` WRITE;
+/*!40000 ALTER TABLE `productstorage` DISABLE KEYS */;
+INSERT INTO `productstorage` VALUES (1,'rio de janeiro',1000),(2,'rio de janeiro',500),(3,'sao paulo',10),(4,'sao paulo',100),(5,'sao paulo',10),(6,'brasilia',60);
+/*!40000 ALTER TABLE `productstorage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productsupplier`
+--
+
+DROP TABLE IF EXISTS `productsupplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productsupplier` (
+  `idPsSupplier` int NOT NULL,
+  `idPsProduct` int NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`idPsSupplier`,`idPsProduct`),
+  KEY `fk_product_supplier_product` (`idPsProduct`),
+  CONSTRAINT `fk_product_supplier_product` FOREIGN KEY (`idPsProduct`) REFERENCES `product` (`idProduct`),
+  CONSTRAINT `fk_product_supplier_supplier` FOREIGN KEY (`idPsSupplier`) REFERENCES `supplier` (`idSupplier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productsupplier`
+--
+
+LOCK TABLES `productsupplier` WRITE;
+/*!40000 ALTER TABLE `productsupplier` DISABLE KEYS */;
+INSERT INTO `productsupplier` VALUES (1,7,500),(1,8,400),(2,9,633),(2,11,10),(3,10,5);
+/*!40000 ALTER TABLE `productsupplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller`
+--
+
+DROP TABLE IF EXISTS `seller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seller` (
+  `idSeller` int NOT NULL AUTO_INCREMENT,
+  `socailName` varchar(255) NOT NULL,
+  `abstName` varchar(255) DEFAULT NULL,
+  `CPF` char(9) DEFAULT NULL,
+  `CNPJ` char(15) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `contact` char(11) NOT NULL,
+  PRIMARY KEY (`idSeller`),
+  UNIQUE KEY `unique_cnpj_seller` (`CNPJ`),
+  UNIQUE KEY `unique_cpf_seller` (`CPF`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seller`
+--
+
+LOCK TABLES `seller` WRITE;
+/*!40000 ALTER TABLE `seller` DISABLE KEYS */;
+INSERT INTO `seller` VALUES (1,'tech eletrocnicos',NULL,NULL,'123456789456321','rio de janeiro','219946287'),(2,'botique durgas',NULL,'123456783',NULL,'rio de janeiro','219567895'),(3,'tech eletrocnicos',NULL,NULL,'456789123654485','sao paulo','1198657484');
+/*!40000 ALTER TABLE `seller` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `storagelocation`
+--
+
+DROP TABLE IF EXISTS `storagelocation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `storagelocation` (
+  `idLproduct` int NOT NULL,
+  `idLstorage` int NOT NULL,
+  `location` varchar(255) NOT NULL,
+  PRIMARY KEY (`idLproduct`,`idLstorage`),
+  KEY `fk_storage_location_storage` (`idLstorage`),
+  CONSTRAINT `fk_storage_location_product` FOREIGN KEY (`idLproduct`) REFERENCES `product` (`idProduct`),
+  CONSTRAINT `fk_storage_location_storage` FOREIGN KEY (`idLstorage`) REFERENCES `productstorage` (`idProdStorage`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `storagelocation`
+--
+
+LOCK TABLES `storagelocation` WRITE;
+/*!40000 ALTER TABLE `storagelocation` DISABLE KEYS */;
+INSERT INTO `storagelocation` VALUES (7,2,'RJ'),(9,6,'GO');
+/*!40000 ALTER TABLE `storagelocation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supplier`
+--
+
+DROP TABLE IF EXISTS `supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supplier` (
+  `idSupplier` int NOT NULL AUTO_INCREMENT,
+  `socialName` varchar(255) NOT NULL,
+  `CNPJ` char(15) NOT NULL,
+  `contact` char(11) NOT NULL,
+  PRIMARY KEY (`idSupplier`),
+  UNIQUE KEY `unique_supplier` (`CNPJ`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supplier`
+--
+
+LOCK TABLES `supplier` WRITE;
+/*!40000 ALTER TABLE `supplier` DISABLE KEYS */;
+INSERT INTO `supplier` VALUES (1,'almeida e filhos','123456789123456','21985474'),(2,'eletronica silva','854519649143457','21985484'),(3,'eletronicos valma','934567893934695','21975474');
+/*!40000 ALTER TABLE `supplier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'ecommerce'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-06 15:17:47
